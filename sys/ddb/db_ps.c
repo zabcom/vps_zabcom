0cf7916144b274b4014c58536f71d706d075b1fb
Björn Zeeb <zabcom@users.noreply.github.com>
2017-07-19 11:38:51 +0000

    Print the ucred as well.

--- sys/ddb/db_ps.c
+++ sys/ddb/db_ps.c
@@ -319,6 +319,7 @@ DB_SHOW_COMMAND(thread, db_show_thread)
 	db_printf(" proc (pid %d): %p\n", td->td_proc->p_pid, td->td_proc);
 	if (td->td_name[0] != '\0')
 		db_printf(" name: %s\n", td->td_name);
+	db_printf(" cred: %p\n", td->td_ucred);
 	db_printf(" stack: %p-%p\n", (void *)td->td_kstack,
 	    (void *)(td->td_kstack + td->td_kstack_pages * PAGE_SIZE - 1));
 	db_printf(" flags: %#x ", td->td_flags);
