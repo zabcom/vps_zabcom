dc01792292f4bd33f8f9fe2093209137a3e839c9
Björn Zeeb <zabcom@users.noreply.github.com>
2017-10-17 15:57:23 +0000

    Script to build bhyve VM images from the live setup.
    Note: the passwords have been publicly documented for year, so
    they are fine to show up here.

--- PATCHES/build-image.sh
+++ PATCHES/build-image.sh
@@ -5,9 +5,25 @@ IMGDIR=/tank/users/bz/vps_zambcom/vps_zabcom/PATCHES
 MKIMG=${INSTPATH}/usr/bin/mkimg
 MKIMG=mkimg
 
-/usr/sbin/makefs -f 10000 -s 8589934592 -D ${IMGDIR}/rootfs.img ${INSTPATH}
+ipmitool -U ADMIN -P ADMIN -I lanplus -H rabbit3-ipmi "power" off
+ipmitool -U ADMIN -P ADMIN -I lanplus -H rabbit4-ipmi "power" off
+
+sleep 60
+
+/usr/sbin/makefs -f 10000 -s 8589934592 -o version=2 -D ${IMGDIR}/rootfs.img ${INSTPATH}
+
+ipmitool -U ADMIN -P ADMIN -I lanplus -H rabbit3-ipmi "power" on
+ipmitool -U ADMIN -P ADMIN -I lanplus -H rabbit4-ipmi "power" on
+
 ${MKIMG} -s bsd -p freebsd-ufs:=${IMGDIR}/rootfs.img -o ${IMGDIR}/disk-vps.img
-cp -fp ${INSTPATH}/boot/kernel/kernel ${IMGDIR}/kernel.vps
+
+echo -n "Waiting for rabbit3 o boot again: "
+while `ping -c 1 -q rabbit3 > /dev/null 2>&1`; do
+	sleep 1
+	echo -n "."
+done
+echo " (30 more seconds)"
+sleep 30
 
 echo -n "Copy over to rabbit3? [Y/n] "
 while read c ; do
@@ -19,6 +35,7 @@ while read c ; do
 	esac
 done
 
-scp -p kernel.vps disk-vps.img test@rabbit3:/mnt/bz/
+ssh test@rabbit3 "su root -c 'mount -t ufs /dev/ada0 /mnt'"
+scp -p disk-vps.img run-bhyve.sh test@rabbit3:/mnt/bz/
 
 # end
